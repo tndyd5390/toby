bb69fb739c34b4af415339b087a854a4f57f2a77
waterdragon <tndyd5390@naver.com>
2023-03-17 01:30:44 +0900

    add HelloController

---
+++ hellobootself/src/main/java/tobyspring/hellobootself/HelloController.java
@@ -0,0 +1,13 @@
+package tobyspring.hellobootself;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
