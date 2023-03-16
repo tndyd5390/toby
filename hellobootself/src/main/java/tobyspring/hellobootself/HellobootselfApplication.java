4a66630b12e0482b505961d470b04673b87ac5e5
waterdragon <tndyd5390@naver.com>
2023-03-17 01:14:56 +0900

    add hellobootself

---
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.hellobootself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobootselfApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobootselfApplication.class, args);
+  }
+
+}
---
+++ hellobootself/src/test/java/tobyspring/hellobootself/HellobootselfApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.hellobootself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootselfApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
