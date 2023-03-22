c87d9b4b8dbeced5f5aad89c7574dfe9f8e271af
waterdragon <tndyd5390@naver.com>
2023-03-22 23:04:23 +0900

    3.7 mapping and binding

--- hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -20,16 +20,20 @@ public class HellobootselfApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     // 인증, 보안, 다국어 처리, 공통 기능처리
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
+                        //처리에 필요한 데이터를 넘겨주는 것을 바인딩이라고 한다.
+                        String ret = helloController.hello(name);
 
                         resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello Servlet " + name);
+                        resp.getWriter().println(ret);
                     } else if (req.getRequestURI().equals("/user")) {
                         //
                     } else {
