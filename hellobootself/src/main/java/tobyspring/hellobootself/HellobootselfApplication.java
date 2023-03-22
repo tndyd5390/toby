ba0d1a38cf202a39c1715fb629cfea8202582e43
waterdragon <tndyd5390@naver.com>
2023-03-22 22:21:38 +0900

    delete annotation

--- hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -1,13 +1,10 @@
 package tobyspring.hellobootself;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class HellobootselfApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(HellobootselfApplication.class, args);
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello containerless standalone Application");
+    }
 
 }
