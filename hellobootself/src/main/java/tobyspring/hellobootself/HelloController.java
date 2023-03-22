a785b33318f50afe923865d7a8a246f511bba236
waterdragon <tndyd5390@naver.com>
2023-03-23 00:51:58 +0900

    4.2 dependency objects

--- hellobootself/src/main/java/tobyspring/hellobootself/HelloController.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HelloController.java
@@ -1,8 +1,12 @@
 package tobyspring.hellobootself;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
