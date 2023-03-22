f697790ab79da1c875a8d7027df29e9112c92040
waterdragon <tndyd5390@naver.com>
2023-03-22 22:46:21 +0900

    3.4 get parameter

--- hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,9 +22,11 @@ public class HellobootselfApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
                     resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello Servlet " + name);
                 }
             }).addMapping("/hello");
         });
