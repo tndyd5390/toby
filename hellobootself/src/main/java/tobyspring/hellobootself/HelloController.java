9f6780400d06ffa164a1bd2894bcdb76dcb4a50f
waterdragon <tndyd5390@naver.com>
2023-03-30 00:14:32 +0900

    4.5 annotation first commit

--- hellobootself/src/main/java/tobyspring/hellobootself/HelloController.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HelloController.java
@@ -1,7 +1,11 @@
 package tobyspring.hellobootself;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    //    @RequestMapping(value = "/hello", method = RequestMethod.GET) -> 예전 방식 추억이다...
+    @GetMapping("/hello")
     public String hello(String name) {
 
         return helloService.sayHello(Objects.requireNonNull(name));
