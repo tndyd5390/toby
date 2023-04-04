6ba57ebd8303222da79c17285a48420ec6aed534
waterdragon <tndyd5390@naver.com>
2023-04-04 23:44:45 +0900

    4.10 spring boot application

--- hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -2,12 +2,10 @@ package tobyspring.hellobootself;
 
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -15,25 +13,9 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootselfApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-
-                webServer.start();
-            }
-        };
-        applicationContext.register(HellobootselfApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(HellobootselfApplication.class, args);
     }
+
 
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
