451b60be81681f018f5d4435a39eb62052615073
waterdragon <tndyd5390@naver.com>
2023-04-04 22:37:41 +0900

    4.8 @Component scan

--- hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -4,12 +4,13 @@ package tobyspring.hellobootself;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class HellobootselfApplication {
 
     public static void main(String[] args) {
@@ -17,26 +18,18 @@ public class HellobootselfApplication {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
+
                 ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
                     servletContext.addServlet("dispatcherServlet",
                             new DispatcherServlet(this)
                     ).addMapping("/*");
                 });
+
                 webServer.start();
             }
         };
         applicationContext.register(HellobootselfApplication.class);
         applicationContext.refresh();
-    }
-
-    @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
-    }
-
-    @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
     }
 }
