961f6b5dcfda7ab026e56a00065b9b6edb976273
waterdragon <tndyd5390@naver.com>
2023-04-17 23:49:29 +0900

    6.2 annotation

--- hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
+++ hellobootself/src/main/java/tobyspring/hellobootself/HellobootselfApplication.java
@@ -1,28 +1,11 @@
 package tobyspring.hellobootself;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class HellobootselfApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootselfApplication.class, args);
-    }
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
     }
 }
--- hellobootself/src/test/java/tobyspring/hellobootself/HelloServiceTest.java
+++ hellobootself/src/test/java/tobyspring/hellobootself/HelloServiceTest.java
@@ -2,31 +2,11 @@ package tobyspring.hellobootself;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
-@Retention(value = RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest
-@interface FastUnitTest {
-
-}
-
-@Retention(value = RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
-@Test
-@interface UnitTest {
-
-}
-
 
 public class HelloServiceTest {
 
-    @FastUnitTest
+    @Test
     void simpleHelloService() {
         //given
         SimpleHelloService helloService = new SimpleHelloService();
