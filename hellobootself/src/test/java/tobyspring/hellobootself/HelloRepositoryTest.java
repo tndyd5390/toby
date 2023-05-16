de49c5869c32cbc558c5350c403cad670ac8a7cf
waterdragon <tndyd5390@naver.com>
2023-05-16 23:55:29 +0900

    9.5 hello repository

---
+++ hellobootself/src/test/java/tobyspring/hellobootself/HelloRepositoryTest.java
@@ -0,0 +1,37 @@
+package tobyspring.hellobootself;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HelloBootSelfTest
+class HelloRepositoryTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    HelloRepository helloRepository;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void findHelloFailed() {
+        assertThat(helloRepository.findHello("sooyong")).isNull();
+    }
+
+    @Test
+    void increaseCount() {
+        assertThat(helloRepository.countOf("sooyong")).isEqualTo(0);
+
+        helloRepository.increaseCount("sooyong");
+        assertThat(helloRepository.countOf("sooyong")).isEqualTo(1);
+
+        helloRepository.increaseCount("sooyong");
+        assertThat(helloRepository.countOf("sooyong")).isEqualTo(2);
+    }
+}
